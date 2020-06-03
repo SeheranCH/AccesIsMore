2bd0c0208162e97ab165924f01493dd22c5a6725
Ivo Gurtner <ivo.gurtner@edu.tbz.ch>
2020-06-03 21:10:51 +0200

    Fix a bug

--- app/src/main/java/ch/noseryoung/accessismore/activities/WelcomeScreenActivity.java
+++ app/src/main/java/ch/noseryoung/accessismore/activities/WelcomeScreenActivity.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import ch.noseryoung.accessismore.R;
+
 public class WelcomeScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
     }
 }
