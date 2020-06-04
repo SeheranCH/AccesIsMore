e8c1e859ccfd5ec64c6370c6255758873e0a8966
Sabrina Westhauser <sabrina.westhauser@edu.tbz.ch>
2020-06-04 16:17:59 +0200

    menu ist erstellt

--- app/src/main/java/ch/noseryoung/accessismore/activities/CreateAccountActivity.java
+++ app/src/main/java/ch/noseryoung/accessismore/activities/CreateAccountActivity.java
@@ -2,17 +2,30 @@ package ch.noseryoung.accessismore.activities;
 
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import ch.noseryoung.accessismore.R;
@@ -39,6 +52,8 @@ public class CreateAccountActivity extends AppCompatActivity {
     private EditText mEditTextEmail;
     private EditText mEditTextPassword1;
     private EditText mEditTextPassword2;
+
+    private ImageView avatarImageView;
 
     private String errorIsNotEmpty;
     private String errorHasMaxLength50;
@@ -69,9 +84,12 @@ public class CreateAccountActivity extends AppCompatActivity {
         Button getAPicture = findViewById(R.id.roundedButton);
         getAPicture.setOnClickListener(mGetAPicture);
 
+        avatarImageView = findViewById(R.id.imgAvatarView);
+
         mUserDAO = AppDatabase.getAppDb(getApplicationContext()).getUserDAO();
 
     }
+
 
     private View.OnClickListener mSaveNewAccount = new View.OnClickListener() {
         @Override
@@ -201,10 +219,13 @@ public class CreateAccountActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
+
+
 }
--- app/src/main/java/ch/noseryoung/accessismore/activities/WelcomeScreenActivity.java
+++ app/src/main/java/ch/noseryoung/accessismore/activities/WelcomeScreenActivity.java
@@ -1,6 +1,10 @@
 package ch.noseryoung.accessismore.activities;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,5 +17,20 @@ public class WelcomeScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_file, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected (MenuItem item) {
+        toasted();
+        return true;
+    }
+
+    public void toasted (){
+        Toast.makeText(this, "True", Toast.LENGTH_SHORT).show();
     }
 }
